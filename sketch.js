806474627f9c0a311bd1f55c7d34d4f95144e68f
KevinAby <78090655+KevinAby@users.noreply.github.com>
2021-02-05 20:07:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var fixedRect,movingRect;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect= createSprite(500, 400, 50, 80);
+  movingRect= createSprite(500, 200, 80, 30);
+
+  fixedRect.velocityY= -6
+  movingRect.velocityY=6
+}
+
+function draw() {
+  background(10);  
+  //movingRect.x=World.mouseX;
+  //movingRect.y=World.mouseY;
+if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 
+  && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2 
+  && movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2 
+  && fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2 )
+  {movingRect.shapeColor="red";
+  fixedRect.shapeColor="red";
+
+}
+else{movingRect.shapeColor="green";
+fixedRect.shapeColor="green";
+
+}
+
+if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 
+  && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2 )
+  {
+    movingRect.velocityX=movingRect.velocityX*(-1)
+    fixedRect.velocityX=movingRect.velocityX*(-1)
+  }
+  if(movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2 
+    && fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2 )
+{
+  movingRect.velocityY=movingRect.velocityY*(-1)
+  fixedRect.velocityY=movingRect.velocityY*(-1)
+}
+  drawSprites();
+}
